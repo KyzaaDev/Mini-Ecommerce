9a823c01d8b5cec7a0cc1bab1dc8097fe10d8a8e
KyzaaDev <dzakyraufnurfhadillah@gmail.com>
2025-07-09 10:24:17 +0700

    update index & conn db

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Selamat Datang</title>
-    <link rel="stylesheet" href="./assets/css/style.css">
+    <link rel="stylesheet" href="./assets/css/indexstyle.css">
 </head>
 <body>
     <?php include("./includes/header.php")?>
