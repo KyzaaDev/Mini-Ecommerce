685152ee67bded407a7b2c063bcf0431c1ae7ebb
KyzaaDev <dzakyraufnurfhadillah@gmail.com>
2025-07-20 02:38:57 +0700

    ga tau nambahin apa sumpah

--- controllers/auth/auth_register.php
+++ controllers/auth/auth_register.php
@@ -4,36 +4,33 @@ require(__DIR__ . "/../../config/db.php");
 if ($_SERVER["REQUEST_METHOD"] === "POST") {
 
     if (isset($_POST['regis'])) {
-        $nama = mysqli_real_escape_string($conn, $_POST["nama"]);
         $email = mysqli_real_escape_string($conn, $_POST["email"]);
         $username = mysqli_real_escape_string($conn, $_POST["username"]);
         $password = $_POST["pass"];
         $passConfirm = $_POST["conf-pass"];
         
-        
         // check panjang password users
         if (strlen($_POST["pass"]) < 8) {
-            header("Location: ../../pages/register.php?error=terlalu_pendek");
-            exit();
-        }
-        
-        // check password confirm
-        if ($password !== $passConfirm) {
-            header("Location: ../../pages/register.php?error=password_tidak_cocok");
+            header("Location: ../../pages/register.php?error=password_too_short");  
             exit();
         }
 
+        // check password confirm
+        if ($password !== $passConfirm) {
+            header("Location: ../../pages/register.php?error=password_not_match");
+            exit();
+        }   
         
         // check input user email
         if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
-            header("Location: ../../pages/register.php?error=invalid");
+            header("Location: ../../pages/register.php?error=invalid_email");
             exit();
         }
 
         // check apakah email sudah terdaftar
         $checkEmail = mysqli_query($conn, "SELECT * FROM users WHERE email = '$email'");
         if(mysqli_num_rows($checkEmail) > 0){
-            header("Location: ../../pages/register.php?error=already_used");
+            header("Location: ../../pages/register.php?error=email_exist");
             exit();
         }
 
@@ -46,7 +43,7 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
         
         //insert data into database
         $passwordHash = password_hash($password, PASSWORD_DEFAULT);
-        $query = "INSERT INTO users VALUES (NULL, '$nama', '$email', '$username', '$passwordHash')";
+        $query = "INSERT INTO users VALUES (NULL, '$email', '$username', '$passwordHash')";
         if (mysqli_query($conn, $query)) {
             header("Location: ../../pages/login.php?register_success=success");
             exit();
