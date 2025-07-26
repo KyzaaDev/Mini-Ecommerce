615a6e38b6e3d4307b8ca3a3f58b90c1d8c80a5f
KyzaaDev <dzakyraufnurfhadillah@gmail.com>
2025-07-26 08:58:01 +0700

    feat: add donation link for support

--- includes/footer.php
+++ includes/footer.php
@@ -1,1 +1,4 @@
-<footer><p>&copy; <?= date('Y');?> KyzaaDev. All rights reserved.</p></footer>
+<footer>
+    <p>&copy; <?= date('Y');?> KyzaaDev. All rights reserved.</p>
+    <p>Jangan lupa dukung dev gadungan ini <a href="https://tako.id/ZaKyzz">di sini!.</a> Thank You!!</p>
+</footer>
